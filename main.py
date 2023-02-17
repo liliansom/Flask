2a142b75364c2ac60ac722e4a83631eac73645d2
Lilian Matsumoto <liliansmatsumoto@gmail.com>
2023-02-17 01:25:32 -0300

    Adicionado decorador @app.route def arq main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/contato')
+def hello_world():
+    return render_template('contato.html')
+
+if __name__ == '__main__':
+    app.run()
