c2148bacb5254398dc8b382d2ddd29df2eba011f
Lilian Matsumoto <liliansmatsumoto@gmail.com>
2023-02-28 00:20:15 -0300

    Inserção de comentários nos arquivos main e home

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 from flask import Flask, render_template, url_for
-from forms import FormLogin
 
-<!-- Criação da página Flask --> 
+# Criação da página Flask
 app = Flask(__name__, static_folder='static' )
 app.config['SECRET_KEY'] = 'e7d749e499af22c81881782c5e957bf0'
 
-<!-- Criação da página homepage --> 
+# Criação da página homepage  
 @app.route('/')
 def home():
     return render_template('home.html')
 
-<!-- Criação da página contato --> 
+# Criação da página contato 
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
 
-<!-- Criação da página respositório --> 
+# Criação da página respositório
 @app.route('/repositorio')
 def repositorio():
     return render_template('repositorio.html')
 
-<!-- Para rodar a página --> 
+# Para rodar a página
 if __name__ == '__main__':
     app.run(debug=True)
     
