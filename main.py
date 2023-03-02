45b623759a0b47856cfdee9873cb8442ff57af34
Lilian Matsumoto <liliansmatsumoto@gmail.com>
2023-03-02 01:00:25 -0300

    Retirada do código secreto do main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for
 
 # Criação da página Flask
 app = Flask(__name__, static_folder='static' )
-app.config['SECRET_KEY'] = 'e7d749e499af22c81881782c5e957bf0'
 
 # Criação da página homepage  
 @app.route('/')
